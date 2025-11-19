c44741e1916bdcb1dd315256020491c2b88e2aa0
min2277 <kingfriedpig@gmail.com>
2025-11-19 17:39:12 +0900

    feat: getPost, getFilterdPost

--- src/api/posts/postRouter.js
+++ src/api/posts/postRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const postService = require('./postService');
 
-router.post('/api/post/test', async (req, res) => {
+router.post('/api/post/upload', async (req, res) => {
     try {
         const payload = req.body;
         const result = await postService.createPost(payload);
@@ -10,6 +10,28 @@ router.post('/api/post/test', async (req, res) => {
     } catch (error) {
         console.error('Error creating post:', error);
         res.status(500).json({ error: 'Error creating post' });
+    }
+});
+
+router.get('/api/post/getpost', async (req, res) => { //page limit    요청을 get post 둘 중 어떤 것으로 할지 고민중입니다.
+    try {
+        const payload = req.query;
+        const result = await postService.getPost(payload);
+        res.status(201).json(result);
+    } catch (error) {
+        console.error('Error get post:', error);
+        res.status(500).json({ error: 'Error get post' });
+    }
+});
+
+router.get('/api/post/getFilteredPost', async (req, res) => { //page limit gerne area
+    try {
+        const payload = req.query;
+        const result = await postService.getFilteredPost(payload);
+        res.status(201).json(result);
+    } catch (error) {
+        console.error('Error get post:', error);
+        res.status(500).json({ error: 'Error get post' });
     }
 });
 
--- src/api/posts/postService.js
+++ src/api/posts/postService.js
@@ -10,7 +10,7 @@ function _post() {
     return getCollection('post');
 }
 
-//새 포스트 작성, 일단 { area, genre, title, content }
+//새 포스트 작성 일단 { area, genre, title, content }
 //이후에 author(작성자 닉네임 등), comment(댓글) 추가
 //likes, views 등 추가할수도? 논의 필요
 async function createPost(payload) {
@@ -21,10 +21,43 @@ async function createPost(payload) {
     }
 
     const result = await _post().insertOne(doc);
-    return { insertedId: result.insertedId, ...doc };
+    return { insertedId: result.insertedId, ...doc };//id가 두번 오는거 같습니다.
 }
-
+//전체 검색 페이징 get요청{page, limit} 
+async function getPost(payload) {
+    const page = parseInt(payload["page"])|| 1;
+    const limit = parseInt(payload["limit"]) || 10;
+    const skip = (page - 1) * limit;
+    const result = await _post().find().skip(skip).limit(limit).toArray(); //최신순으로 바꾸려고 했지만 오류
+    return result;
+}
+//장르, 지역별 get요청 {page, limit,area, genre}
+async function getFilteredPost(payload){
+    const page = parseInt(payload["page"])|| 1;
+    const limit = parseInt(payload["limit"]) || 10;
+    const skip = (page - 1) * limit;
+    const area = payload["area"];
+    const genre = payload["genre"];
+    let query = {};
+    if(area && area != "전국"){
+        query.area = area;
+    }
+    if(genre && genre != "전체"){
+        query.genre = genre;
+    }
+    result = await _post().find(query).skip(skip).limit(limit).toArray();
+    return result;
+}
+//글 삭제 id author (테스트 안해봤습니다)
+async function deletePost(payload) {
+    const post = await _post().findOne({ _id: new ObjectId(payload["id"]) });
+    //if(post["author"] !== payload["author"]){return {error: can't delete}}
+    const result = await _post().deleteOne({ _id: new ObjectId(payload["id"]) });
+    return { result };
+}
 //router에서 호출할 함수들은 여기에 포함해야됨
 module.exports = {
     createPost,
+    getPost,
+    getFilteredPost,
 }
