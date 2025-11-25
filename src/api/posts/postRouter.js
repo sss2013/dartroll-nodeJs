9cb2f2024a36c8b065fa8f980a394528af83052b
min2277 <kingfriedpig@gmail.com>
2025-11-25 22:45:54 +0900

    fix postupload & view & comment

--- src/api/posts/postRouter.js
+++ src/api/posts/postRouter.js
@@ -5,15 +5,16 @@ const postService = require('./postService');
 router.post('/api/post/upload', async (req, res) => {
     try {
         const title = req.body.title;
+        const userId = req.body.userId;
         const area = req.body.area;
         const genre = req.body.genre;
         const content = req.body.content;
         const url = req.body.url;
         const tap = req.body.tap; //review인지 matching인지 구분    
-        if (!title || !area || !genre || !content || !url || !tap) {
+        if (!title || !area || !genre || !content || !url || !tap||!userId) {
             return res.status(400).json({ error: 'Missing required fields' });
         }
-        const payload = { title, area, genre, content, url };
+        const payload = { title, area, genre, content, url,userId };
         const result = await postService.createPost(payload, tap);
         res.status(201).json(result);
     } catch (error) {
@@ -60,5 +61,86 @@ router.get('/api/post/getByArea', async (req, res) => { //page limit gerne area
         res.status(500).json({ error: 'Error get post' });
     }
 });
+//댓글 작성
+router.post('/api/post/:id/comment', async (req, res) => {//postId, userId,text,parentId
+    try {
+        const postId = req.params.id;
+        const userId = req.body.userId;
+        const text = req.body.text;
+        const parentId = req.body.parentId || null; 
+        if (!postId ||!userId ||!text) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+        const payload = {postId,userId,text,parentId}
+        const result = await postService.createComment(payload);
+        res.status(201).json(result);
+    } catch (error) {
+        console.error('Error creating comment:', error);
+        res.status(500).json({ error: 'Error creating comment' });
+    }
+});
+//댓글 가져오기
+router.get('/api/post/:id/comment', async (req, res) => {//postId
+    try {
+        const postId = req.params.id;
+        if (!postId) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+        const payload = {postId}
+        const result = await postService.getComment(payload);
+        res.status(201).json(result);
+    } catch (error) {
+        console.error('Error get comment:', error);
+        res.status(500).json({ error: 'Error get comment' });
+    }
+});
+//view 증가
+router.get('/api/post/:id/views', async (req, res) => { //postId tap
+    try {
+        const postId = req.params.id
+        const tap = req.query.tap;
+        if (!postId||!tap) {
+            return res.status(400).json({ error: 'Missing required query parameters' });
+        }
+        const result = await postService.updateView(postId, tap);
+        res.status(200).json(result);
+    } catch (error) {
+        console.error('Error get post:', error);
+        res.status(500).json({ error: 'Error get post' });
+    }
+});
 
+//댓글 삭제
+router.post('/api/post/:id/commentdelete', async (req, res) => {//_id(댓글), userId
+    try {
+        const id = req.params.id;
+        const userId = req.body.userId;
+        if (!id ||!userId) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+        const payload = {id,userId}
+        const result = await postService.deleteComment(payload);
+        res.status(200).json(result);
+    } catch (error) {
+        console.error('Error deleting comment:', error);
+        res.status(500).json({ error: 'Error deleting comment' });
+    }
+});
+//게시글 삭제
+router.post('/api/post/:id/postdelete', async (req, res) => {//postId, userId
+    try {
+        const id = req.params.id;
+        const userId = req.body.userId; 
+        const tap = req.body.tap;
+        if (!id ||!userId ||!tap) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
+        const payload = {id,userId}
+        const result = await postService.deletePost(payload,tap);
+        res.status(201).json(result);
+    } catch (error) {
+        console.error('Error deleting post:', error);
+        res.status(500).json({ error: 'Error deleting post' });
+    }
+});
 module.exports = router;
--- src/api/posts/postService.js
+++ src/api/posts/postService.js
@@ -9,12 +9,13 @@ const dayjs = require('dayjs');
 function _review() {
     return getCollection('review');
 }
-
 // DB의 matching 테이블(컬렉션) 참조하는 함수
 function _matching() {
     return getCollection('matching');
 }
-
+function _comment() {
+    return getCollection('comment');
+}
 function selectCollection(tap) {
     try {
         switch (tap) {
@@ -29,12 +30,14 @@ function selectCollection(tap) {
     }
 }
 
-//새 포스트 작성 일단 { area, genre, title, content,url  }
+//새 포스트 작성 일단 {userId,area, genre, title, content,url,tap }
 //이후에 author(작성자 닉네임 등), comment(댓글) 추가
 //likes, views 등 추가할수도? 논의 필요
+//views 및 userId 추가
 async function createPost(payload, tap) {
     const doc = {
         ...payload,
+        views: 0,
         createdAt: new Date(),
         updatedAt: new Date(),
     }
@@ -69,16 +72,58 @@ async function getFilteredPost(payload, tap) {
     return result;
 }
 
-//글 삭제 id author (테스트 안해봤습니다)
-async function deletePost(payload) {
-    const post = await _post().findOne({ _id: new ObjectId(payload["id"]) });
-    //if(post["author"] !== payload["author"]){return {error: can't delete}}
-    const result = await _post().deleteOne({ _id: new ObjectId(payload["id"]) });
+//글 삭제 id userId (테스트 안해봤습니다)
+async function deletePost(payload,tap) {
+    const post = await selectCollection(tap).findOne({ _id: new ObjectId(payload.id) });
+    if(post.userId !== payload.userId){return {error: "can't delete"}}
+    const result = await selectCollection(tap).deleteOne({ _id: new ObjectId(payload.id) });
+    //관련댓글 삭제
+    // const comment = await _comment().deleteMany({ postId: new ObjectId(payload.id) });
     return { result };
+}
+//조회수 증가
+async function updateView(postId,tap) {//{글의 id}  views++
+    const id = new ObjectId(postId)
+    const result = await selectCollection(tap).updateOne({ _id:id },{ $inc: { views: 1 } });
+    const post = await selectCollection(tap).findOne({ _id: id});
+    return post;
+}
+//댓글 생성
+async function createComment(payload) {//postId, userId, text, parentId,isDeleted
+    const doc = {
+        ...payload,
+        isDeleted: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    }
+    result = await _comment().insertOne(doc);
+    return result;
+}
+//댓글 삭제
+async function deleteComment(payload) { //댓글 _id, userId,
+    const comment = await _comment().findOne({ _id: new ObjectId(payload.id) });
+    if (!comment) {
+        return { error: "Comment not found" };
+    }
+    if(comment.userId !== payload.userId){return {error: "can't delete"};}
+    const result = await _comment().updateOne({ _id: comment._id },{ $set: { isDeleted: true, content: "삭제된 댓글입니다." } });
+    return { result };
+}
+//댓글 조회
+async function getComment(payload) {//postId postId는 ObjectId가 아님
+    const query = {};
+    query.postId = payload.postId;
+    const result = await _comment().find(query).toArray();
+    return result;
 }
 //router에서 호출할 함수들은 여기에 포함해야됨
 module.exports = {
     createPost,
     getPost,
     getFilteredPost,
+    updateView,
+    createComment,
+    getComment,
+    deleteComment,
+    deletePost,
 }
